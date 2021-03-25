6e3d41f4b75fa069389ba3a9255e06397a6282fd
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-25 13:52:56 +0200

    js commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import { suma } from './components/sum.js';
+import {multiply as daugyba } from './components/matematika.js';
+
+import {sum, divide} from './components/matematika.js';
+
+const rez1 = matematika.suma(8, 5);
+const rez1 = matematika.divide(8, 5);
+
+console.log(rez1);
+console.log(rez4);
